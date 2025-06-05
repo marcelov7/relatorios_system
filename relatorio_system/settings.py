c73193eec0112022fff2a99f5971c08fd3f3a10a
Desenvolvedor <dev@exemplo.com>
2025-06-05 13:17:53 -0300

    Fix ALLOWED_HOSTS in settings.py to use environment variable

--- relatorio_system/settings.py
+++ relatorio_system/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.render.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,.render.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
