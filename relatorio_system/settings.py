437889655222a4d84b8627b9c7a955c20b837922
marcelov7 <marcelov7@gmail.com>
2025-06-22 16:33:06 -0300

    Temp: Desabilitar CSRF middleware temporariamente para resolver problema de acesso

--- relatorio_system/settings.py
+++ relatorio_system/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',  # Temporariamente desabilitado
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
